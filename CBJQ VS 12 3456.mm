42dcc2b9cd950cfa4517ff4cb77cbb7e5d28837f
Menaed <m18210617056@63.com>
2024-05-16 22:55:09 +0000

    更新

--- CBJQ VS 12 3456.mm
+++ CBJQ VS 12 3456.mm
@@ -1,24 +1,24 @@
 <map version="freeplane 1.11.5">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node LOCALIZED_TEXT="new_mindmap" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1606678864093">
+<node TEXT="尘白营销号盗用光与夜之恋素材" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1715898938453" CHILD_NODES_LAYOUT="LEFTTORIGHT_BOTHSIDES_AUTO">
+<icon BUILTIN="yes"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
-<font BOLD="true"/>
-<hook NAME="MapStyle" background="#fdf6e3" zoom="0.826">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_solarized_template.mm"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="0.77">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="true" associatedTemplateLocation="template:/light_nord_template.mm" backgroundImageURI="../Menaeds-GithubFigurebed/Warudo_2023-08-21-20-11-40_1556x854.png"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="default" ID="ID_1704360557" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1704360557" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
-<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
 <richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
 <richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.details">
-<font SIZE="10"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.attributes">
 <font SIZE="10"/>
@@ -27,60 +27,93 @@
 <icon BUILTIN="clock2"/>
 <font SIZE="10"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
 <stylenode LOCALIZED_TEXT="defaultstyle.floating">
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
 </stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1083719472" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
 <icon BUILTIN="yes"/>
-<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1083719472"/>
-<font SIZE="12" BOLD="true"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
 <edge COLOR="#bf616a"/>
 </stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
 <font SIZE="18"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
 <font SIZE="16"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
 <font SIZE="14"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
 <font SIZE="12"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
 <font SIZE="11"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
 <font SIZE="11"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
 <font SIZE="10"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
 <font SIZE="10"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
 <font SIZE="10"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
 <font SIZE="10"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
-<font SIZE="9"/>
-</stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#fdf6e3">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
 <font SIZE="9"/>
 </stylenode>
 </stylenode>
 </stylenode>
 </map_styles>
 </hook>
+<node TEXT="历史" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="bottom_or_right" ID="ID_1641041637" CREATED="1715897406097" MODIFIED="1715897658248" HGAP_QUANTITY="-134.5 pt" VSHIFT_QUANTITY="-248.99999 pt">
+<icon BUILTIN="emoji-1F4D4"/>
+<node TEXT="123流厂商" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" FOLDED="true" ID="ID_607475690" CREATED="1715897437059" MODIFIED="1715898960814" HGAP_QUANTITY="81.5 pt" VSHIFT_QUANTITY="9 pt">
+<icon BUILTIN="emoji-1F913"/>
+<arrowlink DESTINATION="ID_1566047932" TARGET_LABEL="回旋标" MIDDLE_LABEL="AM2算半夜吗" STARTINCLINATION="3 pt;18 pt;" ENDINCLINATION="-7.5 pt;-27 pt;"/>
+<node TEXT="" ID="ID_1945018647" CREATED="1715897747616" MODIFIED="1715897758682" HGAP_QUANTITY="80 pt" VSHIFT_QUANTITY="-93.75 pt"/>
+</node>
+<node TEXT="ZZZ" ID="ID_1566047932" CREATED="1715897773959" MODIFIED="1715897780249" HGAP_QUANTITY="173 pt" VSHIFT_QUANTITY="74.25 pt"/>
+</node>
+<node TEXT="光夜玩家" POSITION="bottom_or_right" ID="ID_652458452" CREATED="1715897173337" MODIFIED="1715898995938" HGAP_QUANTITY="-321.99999 pt" VSHIFT_QUANTITY="788.24998 pt">
+<node TEXT="CBJQ=插不进去" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1356894050" CREATED="1715898209229" MODIFIED="1715899121899" HGAP_QUANTITY="-163.74999 pt" VSHIFT_QUANTITY="884.24997 pt" TEXT_ALIGN="LEFT"/>
+<node TEXT="不当言论" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_173798794" CREATED="1715898258647" MODIFIED="1715899104670" HGAP_QUANTITY="5.75 pt" VSHIFT_QUANTITY="-752.99998 pt">
+<icon BUILTIN="help"/>
+<arrowlink DESTINATION="ID_841977520" STARTINCLINATION="-3.75 pt;12.75 pt;" ENDINCLINATION="3.75 pt;-27.75 pt;"/>
+</node>
+</node>
+<node TEXT="仙" POSITION="top_or_left" ID="ID_1866682187" CREATED="1715898530368" MODIFIED="1715900020739" HGAP_QUANTITY="264.49999 pt" VSHIFT_QUANTITY="666.74998 pt">
+<arrowlink DESTINATION="ID_1356894050" MIDDLE_LABEL="好用" STARTINCLINATION="-36 pt;0 pt;" ENDINCLINATION="-229.49999 pt;-14.25 pt;"/>
+<node TEXT="仙舟联盟" ID="ID_1355939496" CREATED="1715899616988" MODIFIED="1715900074263" HGAP_QUANTITY="172.25 pt" VSHIFT_QUANTITY="102.75 pt">
+<icon BUILTIN="emoji-1F9D0"/>
+<arrowlink DESTINATION="ID_1727232337" MIDDLE_LABEL="TTK" STARTINCLINATION="-71.25 pt;31.5 pt;" ENDINCLINATION="28.5 pt;-42.75 pt;"/>
+<arrowlink DESTINATION="ID_1356894050" STARTINCLINATION="-26.25 pt;45 pt;" ENDINCLINATION="-9.75 pt;-24 pt;"/>
+<arrowlink DESTINATION="ID_1727232337" MIDDLE_LABEL="质疑买量进而得出尘白盗用素材" STARTINCLINATION="28.5 pt;20.25 pt;" ENDINCLINATION="27.75 pt;-35.25 pt;"/>
+</node>
+</node>
+<node TEXT="尘白玩家" POSITION="top_or_left" ID="ID_1727232337" CREATED="1715898645401" MODIFIED="1715899593581" HGAP_QUANTITY="87.5 pt" VSHIFT_QUANTITY="366.74999 pt">
+<arrowlink DESTINATION="ID_1866682187" MIDDLE_LABEL="关系不好" STARTINCLINATION="247.49999 pt;0 pt;" ENDINCLINATION="-33 pt;45 pt;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+<node TEXT="尘白玩家PTSD" ID="ID_841977520" CREATED="1715898682687" MODIFIED="1715899933603" HGAP_QUANTITY="17.75 pt" VSHIFT_QUANTITY="108 pt">
+<arrowlink DESTINATION="ID_652458452" MIDDLE_LABEL="TTK" STARTINCLINATION="32.25 pt;0 pt;" ENDINCLINATION="42.75 pt;42 pt;"/>
+<node TEXT="12,3456" ID="ID_1449799455" CREATED="1715899320031" MODIFIED="1715899514768" HGAP_QUANTITY="119 pt" VSHIFT_QUANTITY="306.74999 pt">
+<arrowlink DESTINATION="ID_1866682187" STARTINCLINATION="811.49998 pt;0 pt;" ENDINCLINATION="-54.75 pt;-2.25 pt;"/>
+<arrowlink DESTINATION="ID_1356894050" MIDDLE_LABEL="TTK" STARTINCLINATION="144 pt;-38.25 pt;" ENDINCLINATION="128.25 pt;27 pt;"/>
+<arrowlink DESTINATION="ID_1866682187" STARTINCLINATION="51 pt;-86.25 pt;" ENDINCLINATION="-136.5 pt;-203.24999 pt;"/>
+</node>
+</node>
+</node>
 </node>
 </map>
